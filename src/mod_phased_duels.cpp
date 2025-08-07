829b1614f12667478037b7eb342abf966c406e85
Takenbacon <revoke1336@live.com>
2025-08-07 09:32:09 -0700

    Update mod_phased_duels.cpp

--- src/mod_phased_duels.cpp
+++ src/mod_phased_duels.cpp
@@ -39,7 +39,7 @@ void PhasedDueling::OnPlayerDuelStart(Player* firstplayer, Player* secondplayer)
         std::list<Player*> playerList;
         Acore::AnyPlayerInObjectRangeCheck checker(go, 100.0f);
         Acore::PlayerListSearcher<Acore::AnyPlayerInObjectRangeCheck> searcher(go, playerList, checker);
-        Cell::VisitWorldObjects(go, searcher, 100.0f);
+        Cell::VisitObjects(go, searcher, 100.0f);
 
         // insert players' phases to used phases, ignore GMs
         uint32 usedPhases = 0;
